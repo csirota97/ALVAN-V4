ee48b50e2d94d55c2dfa9f7e5980bac38039ad33
Craig Sirota <csirota97@gmail.com>
2022-06-12 00:51:48 -0400

    Updates

--- src/js/App.js
+++
@@ -1,7 +0,0 @@
-import Screen from './components/screen';
-
-function App() {
-  return (<Screen />);
-}
-
-export default App;
--- src/js/utils/constants.js
+++
@@ -1,7 +0,0 @@
-const constants = {
-  LISTENER_MIC_TIMEOUT: 10000,
-  LISTENER_MIC_LANG: 'en-US',
-  ALVAN_MIC_TIMEOUT: 5000,
-};
-
-export default constants;
