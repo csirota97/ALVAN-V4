88feb82fe3419e318e9cb821a0072a17efba9e0b
Craig Sirota <34724663+csirota97@users.noreply.github.com>
2022-06-24 08:17:39 -0400

    AV-22 Added min and max temp to daily forecasts (#17)
    Co-authored-by: Craig Sirota <csirota97@gmail.com>

--- ALVAN_HUB_UI/alvan-hub-ui/tests/jest/components/weatherCalendarView.test.js
+++ ALVAN_HUB_UI/alvan-hub-ui/tests/jest/components/weatherCalendarView.test.js
@@ -1,6 +1,6 @@
 import 'jsdom-global/register';
 import React from "react";
-// import SpeechRecognizer from '../../../src/js/components/speechRecognizer';
+import { act } from 'react-dom/test-utils';
 import WeatherCalendarView from '../../../src/js/components/weatherCalendarView';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { mount, configure, EnzymeAdapter } from "enzyme";
@@ -9,14 +9,20 @@ import { Watch } from 'react-loader-spinner';
 
 configure({adapter: new Adapter()});
 
-jest.mock('../../../src/js/utils/service-factory.js');
 describe('WeatherCalendarView', () => {
   let WCVWrapper;
   let mockTrigger = false;
   const mockSetDefaultWeather = jest.fn();
   const mockDefaultWeather = mockWeatherCall;
   beforeEach(() => {
+    console.log(`\n\n\n${process.env.NODE_ENV}\n\n\n`);
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(new Date(2022, 6, 21));
     WCVWrapper = mount(<WeatherCalendarView trigger={mockTrigger} defaultWeather={mockDefaultWeather} setDefaultWeather={mockSetDefaultWeather} />)
+  });
+
+  afterAll(() => {
+      jest.useRealTimers();
   });
 
   describe('should render', () => {
@@ -34,30 +40,54 @@ describe('WeatherCalendarView', () => {
         noDataWrapper = mount(<WeatherCalendarView trigger={mockTrigger} setDefaultWeather={mockSetDefaultWeather} />);
       })
 
-      it('should display loading indicators', () => {
+      it('should render loading indicators', () => {
         expect(noDataWrapper.find(Watch).length).toBe(4);
       });
 
-      it('should not show any current weather data', () => {
+      it('should not render any current weather data', () => {
         expect(noDataWrapper.find('#current-data').length).toBe(0);
       });
     });
 
     describe('data has been recieved', () => {
-      it('should not display loading indicators', () => {
+      it('should not render loading indicators', () => {
         expect(WCVWrapper.find(Watch).length).toBe(0);
       });
 
-      it('should display weather indicators', () => {
+      it('should render weather indicators', () => {
         expect(WCVWrapper.find('.weather-icon').length).toBe(4);
       });
 
-      it('should show current weather data', () => {
+      it('should render current weather data', () => {
         expect(WCVWrapper.find('#current-data').length).toBe(1);
       });
 
-      it('should show current weather data text', () => {
+      it('should render current weather data text', () => {
         expect(WCVWrapper.find('#current-data').text()).toBe("OvercastTemp: 66.9°FFeels Like: 66.9°FWind Speed: 9.4 mph");
+      });
+
+      it('should render 3 min temperatures', () => {
+        expect(WCVWrapper.find('.min-temp').length).toBe(3);
+      });
+
+      it('should render 3 max temperatures', () => {
+        expect(WCVWrapper.find('.max-temp').length).toBe(3);
+      });
+
+      it('should have correct first min temperatures', () => {
+        expect(WCVWrapper.find('.min-temp').first().text()).toBe("Min: 61.3°F");
+      });
+
+      it('should have correct first max temperatures', () => {
+        expect(WCVWrapper.find('.max-temp').first().text()).toBe("Max: 67.5°F");
+      });
+
+      it('should have correct last min temperatures', () => {
+        expect(WCVWrapper.find('.min-temp').last().text()).toBe("Min: 63.9°F");
+      });
+
+      it('should have correct last max temperatures', () => {
+        expect(WCVWrapper.find('.max-temp').last().text()).toBe("Max: 89.2°F");
       });
     });
   });
