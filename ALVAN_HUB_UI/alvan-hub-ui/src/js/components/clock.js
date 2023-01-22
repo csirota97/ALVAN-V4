5ca5dd39e81e3fe238b3188cf857223079e82baa
csirota97 <csirota97@gmail.com>
2023-01-22 00:53:23 -0500

    Quick refresh trigger

--- ALVAN_HUB_UI/alvan-hub-ui/src/js/components/clock.js
+++ ALVAN_HUB_UI/alvan-hub-ui/src/js/components/clock.js
@@ -9,7 +9,7 @@ function Clock () {
     },1000);
   });
 
-  return (<h1 className="clock nonselectable">{time}</h1>);
+  return (<h1 className="clock nonselectable" onClick={() => window.location.reload()}>{time}</h1>);
 }
 
 export default Clock;
